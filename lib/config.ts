89832834fd50d26b916c36ad5fbf3cf1ca92bf86
narayani-org <connect@narayani.org>
2026-07-21 02:47:44 +0530

    w

--- lib/config.ts
+++ lib/config.ts
@@ -1,4 +1,4 @@
 export const API_BASE =
-  process.env.NEXT_PUBLIC_API_BASE || "http://localhost:8000/api";
+  process.env.NEXT_PUBLIC_API_BASE || "http://162.0.214.158:8080/api";
 
 export const TOKEN_KEY = "satta_admin_token";
